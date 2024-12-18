90f6c61b6bb5d323bd49cb0031288dd00745444c
IFYates <ian@iyates.co.uk>
2024-12-18 15:21:59 +0000

    Correct falsy/null behaviour of bound attributes

--- __tests__/attributes.tests.js
+++ __tests__/attributes.tests.js
@@ -19,20 +19,20 @@ test('Attribute templates on bound elements are resolved', () => {
   expect(document.body.innerHTML).toBe('<div class="attribute"></div>')
 })
 
-test('Attribute falsy templates on unbound elements are resolved', () => {
+test.each([[false, 'false'], [0, '0'], [null, ''], [undefined, ''], ['', '']])('Attribute falsy templates on unbound elements are resolved', (value, expected) => {
   document.body.innerHTML = '<div class="{{ value }}"></div>'
 
-  pow.apply(document.body, { value: false })
+  pow.apply(document.body, { value })
 
-  expect(document.body.innerHTML).toBe('<div class="false"></div>')
+  expect(document.body.innerHTML).toBe(`<div class="${expected}"></div>`)
 })
 
-test('Attribute falsy templates on bound elements are removed', () => {
+test.each([[false, ' class="false"'], [0, ' class="0"'], [null, ''], [undefined, ''], ['', '']])('Attribute falsy templates on bound elements are removed', (value, expected) => {
   document.body.innerHTML = '<div pow class="{{ value }}"></div>'
 
-  pow.apply(document.body, { value: false })
+  pow.apply(document.body, { value })
 
-  expect(document.body.innerHTML).toBe('<div></div>')
+  expect(document.body.innerHTML).toBe(`<div${expected}></div>`)
 })
 
 test('$attributes on bound elements are resolved', () => {
@@ -51,10 +51,10 @@ test('$attributes on unbound elements are not resolved', () => {
   expect(document.body.innerHTML).toBe('<div $class="value"></div>')
 })
 
-test('falsy $attributes on bound elements are removed', () => {
+test.each([[false], [0], [null], [undefined], ['']])('falsy $attributes on bound elements are removed', (value) => {
   document.body.innerHTML = '<div pow $class="value"></div>'
 
-  pow.apply(document.body, { value: false })
+  pow.apply(document.body, { value })
 
   expect(document.body.innerHTML).toBe('<div></div>')
 })
@@ -67,10 +67,10 @@ test('$attributes on bound elements replace existing', () => {
   expect(document.body.innerHTML).toBe('<div class="attribute"></div>')
 })
 
-test('falsy $attributes on bound elements leave existing', () => {
+test.each([[false], [0], [null], [undefined], ['']])('falsy $attributes on bound elements leave existing', (value) => {
   document.body.innerHTML = '<div pow class="existing" $class="value"></div>'
 
-  pow.apply(document.body, { value: false })
+  pow.apply(document.body, { value })
 
   expect(document.body.innerHTML).toBe('<div class="existing"></div>')
 })
