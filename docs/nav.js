6fe13a16cafc796a595b17482a5923f4e85c7e2d
Ian Yates <ian.yates@neofintechlab.com>
2025-01-01 22:16:42 +0000

    Completed docs

--- docs/nav.js
+++ docs/nav.js
@@ -7,18 +7,20 @@ window.refreshMain = () => {
     // TODO: scroll to top
 }
 
-window.activeVersion = 2.0
+window.activeVersion = 1.4 //2.0
 window.setActiveVersion = function(context) {
     window.activeVersion = context.$data
     window.refreshMain()
 }
 window.version = (firstVersion, lastVersion) => activeVersion >= firstVersion && (!lastVersion || activeVersion < lastVersion)
 
+window.fn = (value) => typeof value == 'function' ? value() : value
+
 const nav = {
     current: location.search?.match(/^\?\/([^\/]+)\/?$/) ? RegExp.$1 : 'home',
     navigate: function (item) {
         this.href = 'javascript:void(0)'
-        nav.current = item.id
+        nav.current = item.id ?? item
         history.pushState(null, null, `?/${item.id}/`)
         refreshMain()
         return false
@@ -29,11 +31,12 @@ const nav = {
     },
 
     pages: [
-        { id: 'home', name: 'Home', icon: '<i class="bi-house-door"></i>' },
-        { id: 'get-started', name: 'Getting started' },
+        { id: 'home', name: 'Home', icon: 'fas fa-home' },
+        { id: 'get-started', name: 'Getting started', icon: 'fas fa-graduation-cap' },
         {
             id: 'syntax',
             name: 'Syntax',
+            icon: 'fas fa-code',
             children: [
                 { id: 'syntax-binding', name: 'Bindings' },
                 { id: 'syntax-expressions', name: 'Expressions' },
@@ -43,9 +46,10 @@ const nav = {
         {
             id: 'bindings',
             name: 'Bindings',
+            icon: 'fas fa-link',
             children: [
                 { id: 'bindings-loops', name: 'array' },
-                { id: 'bindings-data', name: 'data / item', url: '?/syntax-binding/#data' },
+                { id: 'bindings-data', name: () => activeVersion >= 2.0 ? 'data' : 'item', url: '?/syntax-binding/#data' },
                 { id: 'bindings-conditions', name: 'if / ifnot / else' },
                 { id: 'bindings-stop', name: 'stop', url: '?/syntax-binding/#stop' },
                 { id: 'bindings-templates', name: 'template' },
@@ -54,22 +58,23 @@ const nav = {
         {
             id: 'features',
             name: 'Features',
+            icon: 'fas fa-cogs',
             children: [
                 { id: 'features-interaction', name: 'Interactivity' },
                 { id: 'features-reactivity', name: 'Reactivity' },
                 { id: 'features-pow-safe', name: 'pow.safe' },
             ]
         },
-        {
-            id: 'examples',
-            name: 'Examples',
-            children: [
-                { id: 'examples-quickstart', name: 'Quickstart' },
-            ]
-        },
-        { id: 'breaking-changes', name: 'Breaking changes' },
-        { id: 'troubleshooting', name: 'Troubleshooting' },
-        { id: 'repo', name: 'Repository', url: 'https://github.com/IFYates/pow.js' }
+        // {
+        //     id: 'examples',
+        //     name: 'Examples',
+        //     children: [
+        //         { id: 'examples-quickstart', name: 'Quickstart' },
+        //     ]
+        // },
+        { id: 'breaking-changes', name: 'Breaking changes', icon: 'fas fa-exclamation-triangle' },
+        { id: 'troubleshooting', name: 'Troubleshooting', icon: 'fas fa-bug' },
+        { id: 'repo', name: 'Repository', url: 'https://github.com/IFYates/pow.js', icon: 'fab fa-github' },
     ]
 }
 console.log(nav.current)
