ad29f455fd750d84c26010eeaaa8a07e58985ab8
IFYates <ian@iyates.co.uk>
2025-01-06 09:46:02 +0000

    Doc fix

--- publish.ps1
+++ publish.ps1
@@ -6,19 +6,19 @@ param (
 $dest = "$PSScriptRoot/../ifyates.github.io/pow.js"
 @('coverage', 'examples', 'src') | ForEach-Object {
     Remove-Item -Path "$dest/$_" -Recurse -Force
-    Copy-Item -Path $_ -Recurse -Destination "$dest/$_"
+    Copy-Item -Path "$PSScriptRoot/$_" -Recurse -Destination "$dest/$_"
 }
 
 # Keep version
-$package = (ConvertFrom-Json (Get-Content ./package.json -Raw))
+$package = (ConvertFrom-Json (Get-Content "$PSScriptRoot/package.json" -Raw))
 Remove-Item -Path "$dest/v$($package.version)" -Recurse -Force -ErrorAction Ignore
-Copy-Item -Path "dist" -Recurse -Destination "$dest/v$($package.version)"
+Copy-Item -Path "$PSScriptRoot/dist" -Recurse -Destination "$dest/v$($package.version)"
 Remove-Item -Path "$dest/latest" -Recurse -Force
-Copy-Item -Path "dist" -Recurse -Destination "$dest/latest"
+Copy-Item -Path "$PSScriptRoot/dist" -Recurse -Destination "$dest/latest"
 
 # Documentation
 Remove-Item -Path "$dest/docs" -Recurse -Force
-Copy-Item -Path "docs" -Recurse -Destination "$dest/docs"
+Copy-Item -Path "$PSScriptRoot/docs" -Recurse -Destination "$dest/docs"
 
 # NPM
 if ($GoLive) {
