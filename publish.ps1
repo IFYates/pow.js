e2a36c7d0c5d24bb76f4813521cb982f23a1ed08
Ian Yates <ian.yates@neofintechlab.com>
2025-01-06 13:17:56 +0000

    Dropped bug revision from dist versions

--- publish.ps1
+++ publish.ps1
@@ -11,8 +11,9 @@ $dest = "$PSScriptRoot/../ifyates.github.io/pow.js"
 
 # Keep version
 $package = (ConvertFrom-Json (Get-Content "$PSScriptRoot/package.json" -Raw))
-Remove-Item -Path "$dest/v$($package.version)" -Recurse -Force -ErrorAction Ignore
-Copy-Item -Path "$PSScriptRoot/dist" -Recurse -Destination "$dest/v$($package.version)"
+$version = $package.version.Split('.')[0..1] -join '.'
+Remove-Item -Path "$dest/v$version" -Recurse -Force -ErrorAction Ignore
+Copy-Item -Path "$PSScriptRoot/dist" -Recurse -Destination "$dest/v$version"
 Remove-Item -Path "$dest/latest" -Recurse -Force
 Copy-Item -Path "$PSScriptRoot/dist" -Recurse -Destination "$dest/latest"
 
