d2095c79c80e1e118667b07e9cbcc40076f96df2
Ian Yates <ian@iyates.co.uk>
2025-01-10 12:19:43 +0000

    Some code tidy

--- src/pow.js
+++ src/pow.js
@@ -11,32 +11,32 @@ const B_TEMPLATE = 'template', B_TRANSFORM = 'transform'
 const CONTENT = 'content', FUNCTION = 'function', INN_HTML = 'innerHTML', OUT_HTML = 'outerHTML', REPLACE = 'replace'
 
 const _attr = { set: (el, name, value) => el.setAttribute(name, value), rem: (el, name) => el.removeAttribute(name) }
-const _escape = (text, asRoot) => asRoot ? text : text[REPLACE](/({|p)({|ow)/g, '$1​$2​')
-const _randomId = () => '_' + Math.random().toString(36).slice(2)
+const _escape = (text, skip) => skip ? text : text[REPLACE](/({|p)({|ow)/g, '$1​$2​')
+const _randomId = _ => '_' + Math.random().toString(36).slice(2)
 const _replace = (el, html) => el.replaceWith(document.createRange().createContextualFragment(html)) // TODO: better solution
 const _selectChild = (el, selector) => (el[CONTENT] ?? el).querySelectorAll(selector)
 
-const processElement = (element, state, isRoot, val) => {
+const processElement = (element, state, isRoot, _val) => {
     // Interpolates text templates
     const parseText = (text) => _escape(text[REPLACE](/{{\s*(.*?)\s*}}/gs, (_, expr) => parseExpr(expr) ?? ''), isRoot)
 
     // Allow sibling 'else' based on condition
-    const processCondition = (active, alwaysRemove, sibling = element.nextElementSibling) => {
-        if (!active && sibling?.attributes.pow)
-            _attr.rem(sibling, B_ELSE)
+    const processCondition = (active, alwaysRemove, _sibling = element.nextElementSibling) => {
+        if (!active && _sibling?.attributes.pow)
+            _attr.rem(_sibling, B_ELSE)
         return (alwaysRemove | !active) && !element.remove()
     }
 
     // Resolves an expression to a value
-    const parseExpr = (expr, raw, context = getContext(state)) => {
+    const parseExpr = (expr, raw, _context = getContext(state)) => {
         try {
             // Execute the expression as JS code, mapping to the state data
-            const value = pow._eval(expr, context)
+            const value = pow._eval(expr, _context)
 
             // If the result is a function, bind it for later
             if (!raw & typeof value == FUNCTION) {
                 raw = _randomId()
-                window[state.$id][raw] = (el) => value.call(el, context)
+                window[state.$id][raw] = (el) => value.call(el, _context)
                 return `${state.$id}.${raw}(this)`
             }
             return value
@@ -44,8 +44,10 @@ const processElement = (element, state, isRoot, val) => {
             console.warn('Interpolation failed', { [P_PATH]: state[P_PATH], expr }, e)
         }
     }
-    const getContext = (state) => ({ ...state[P_DATA], ...state,
-        [P_PARENT]: state[P_PARENT] && getContext(state[P_PARENT]) })
+    const getContext = (state) => ({
+        ...state[P_DATA], ...state,
+        [P_PARENT]: state[P_PARENT] && getContext(state[P_PARENT])
+    })
 
     // Prepare custom elements
     for (const el of [..._selectChild(element, '*')].filter($ => $.tagName.startsWith('POW:')))
@@ -63,9 +65,9 @@ const processElement = (element, state, isRoot, val) => {
         _attr.rem(element, name)
 
         // Apply template
-        if (name == B_TEMPLATE && !processCondition(val = document.getElementById(value))) {
+        if (name == B_TEMPLATE && !processCondition(_val = document.getElementById(value))) {
             // Gather content data
-            let content = { }, defaultContent = null
+            let content = {}, defaultContent = null
             for (const child of _selectChild(element, B_TEMPLATE)) {
                 defaultContent ??= child[INN_HTML] // Remember first template
                 content[child.id] = child[INN_HTML]
@@ -74,7 +76,7 @@ const processElement = (element, state, isRoot, val) => {
 
             // Build content with replaced params
             // Has to be a regex on full HTML due to template contents not being in DOM
-            element[INN_HTML] = val[INN_HTML][REPLACE](/<param(?:\s+id=["']([^"']+)["'])?\s*\/?>/g,
+            element[INN_HTML] = _val[INN_HTML][REPLACE](/<param(?:\s+id=["']([^"']+)["'])?\s*\/?>/g,
                 // Find templates by id or default first
                 // Default param (no id), takes whole content if there were no templates
                 (_, id) => id ? (content[id] || '') : defaultContent)
@@ -83,48 +85,48 @@ const processElement = (element, state, isRoot, val) => {
         }
 
         // Some logic requires resolved expressions
-        val = () => val = (value ? parseExpr(value) : state[P_DATA])
+        _val = _ => _val = (value ? parseExpr(value) : state[P_DATA])
 
         if (name == B_ARRAY) { // Element loop
-            val = !val() | Array.isArray(val) ? val
-                : Object.entries(val).map(([key, value]) => ({ key, value }))
-            for (let i = 0; i < val?.length; ++i) {
+            _val = !_val() | Array.isArray(_val) ? _val
+                : Object.entries(_val).map(([key, value]) => ({ key, value }))
+            for (let i = 0; i < _val?.length; ++i) {
                 const child = element.cloneNode(1)
                 element.parentNode.insertBefore(child, element)
                 processElement(child, {
                     ...state, [P_PATH]: `${state[P_PATH]}.${value || B_ARRAY}[${i}]`, $index: i,
-                    $first: !i, $last: i > val.length - 2, [P_DATA]: val[i], $array: val, [P_PARENT]: state
+                    $first: !i, $last: i > _val.length - 2, [P_DATA]: _val[i], $array: _val, [P_PARENT]: state
                 })
             }
-            return processCondition(val?.length, 1)
+            return processCondition(_val?.length, 1)
         } else if (name == B_DATA) { // Data binding
-            return processCondition(val() != null)
+            return processCondition(_val() != null)
                 ? 0 // Removed as inactive
                 : processElement(element, {
                     ...state, [P_PATH]: state[P_PATH] + '.' + value,
-                    [P_DATA]: val, [P_PARENT]: state
+                    [P_DATA]: _val, [P_PARENT]: state
                 }, isRoot)
         } else if (name == B_IF | name == B_IFNOT) { // Conditional element
-            if (processCondition((name == B_IF) != !val()))
+            if (processCondition((name == B_IF) != !_val()))
                 return // Removed as inactive
         } else if (name[0] == ':') { // Interpolated attribute
-            if (val())
-                _attr.set(element, name.slice(1), val)
-            return processElement(element, state)
+            if (_val())
+                _attr.set(element, name.slice(1), _val)
+            return processElement(element, state, isRoot)
         } else if (name.at(-1) == ':') { // Data attribute
-            state = { ...state, [P_DATA]: { ...state[P_DATA], [name.slice(0, -1)]: val() } }
+            state = { ...state, [P_DATA]: { ...state[P_DATA], [name.slice(0, -1)]: _val() } }
         } else if (name == B_ELSE) { // If 'else' survives to here, the element isn't wanted
             return element.remove()
         } else if (name == B_TRANSFORM) { // Transformation function
             transformFunction = parseExpr(value, 1)
-        } else if (val = parseText(value)) { // Standard attribute
-            _attr.set(element, name, val)
+        } else if (_val = parseText(value)) { // Standard attribute
+            _attr.set(element, name, _val)
         }
     }
 
     // Process every child 'pow' template
-    while (val = _selectChild(element, '[pow]:not([pow] [pow])')[0])
-        processElement(val, state)
+    while (_val = _selectChild(element, '[pow]:not([pow] [pow])')[0])
+        processElement(_val, state)
 
     // Transform complete element
     if (typeof transformFunction == FUNCTION)
@@ -146,10 +148,11 @@ const bind = (element) => {
                 return console.warn('Binding already in progress')
             binding.$ = 1
 
-            // Reset global state
-            element[INN_HTML] = originalHTML
-            attributes.forEach($ => _attr.set(element, $.name, $.value))
-            window[$id] = {}
+        // Reset global state
+        element[INN_HTML] = originalHTML
+        for (const attr of attributes)
+            _attr.set(element, attr.name, attr.value)
+        window[$id] = {}
 
             try {
                 processElement(element, { $id, [P_PATH]: P_ROOT, [P_DATA]: data, [P_ROOT]: data }, 1)
@@ -169,7 +172,7 @@ const bind = (element) => {
 const pow = {
     apply: (element, data) => bind(element).apply(data),
     bind,
-    _eval: (expr, ctxt, args = Object.entries(ctxt).filter($ => isNaN($[0]))) =>
-        (new Function(...args.map($ => $[0]), 'return ' + expr)).call(ctxt[P_DATA], ...args.map($ => $[1]))
+    _eval: (expr, ctxt, _args = Object.entries(ctxt).filter($ => isNaN($[0]))) =>
+        (new Function(..._args.map($ => $[0]), 'return ' + expr)).call(ctxt[P_DATA], ..._args.map($ => $[1]))
 }
 export default pow
